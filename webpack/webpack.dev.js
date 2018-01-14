1e929a34c91b20649664bb0cab33498da1583e7a
Gogs <gogs@fake.local>
2018-01-14 12:39:05 +0800

    update

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,10 +1,19 @@
+const path = require('path');
 const webpack = require('webpack');
 const merge = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
 
 const common = require('./webpack.common.js');
 const config = require('./config.js')
+
+function assetsPath(_path) {
+    var assetsSubDirectory = process.env.NODE_ENV === 'production'
+        ? config.build.assetsSubDirectory
+        : config.dev.assetsSubDirectory
+    return path.posix.join(assetsSubDirectory, _path)
+}
 
 module.exports = merge(common, {
     devtool: '#cheap-module-eval-source-map',
@@ -19,6 +28,7 @@ module.exports = merge(common, {
             template: 'index.html',
             inject: true
         }),
+        new ExtractTextPlugin({filename: assetsPath('css/app.css')}),
         new webpack.optimize.CommonsChunkPlugin({
             name: 'vendor' // 指定公共 bundle 的名称。
         }),
